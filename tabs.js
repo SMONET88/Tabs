3c47de689e815161c336254a43d6865ae2cc80fb
SMONET88 <145613729+SMONET88@users.noreply.github.com>
2025-03-21 15:44:29 -0500

    first draft

---
+++ tabs.js
@@ -0,0 +1,11 @@
+const tabOne = document.getElementById('tab1');
+const tabTwo = document.getElementById('tab2');
+const tabThree = document.getElementById('tab3');
+const tabFour = document.getElementById('tab4');
+
+
+
+
+tabOne.addEventListener('click', function(){
+    alert("tab 1 clicked!");
+});
